69158e9ba9ce9d5bea048e0c1e5577733670e4dd
dkodurul <dkodurul@kent.edu>
2024-09-29 10:17:39 -0400

    new database

--- llama3.py
+++ llama3.py
@@ -17,7 +17,7 @@ class LLaMA3:
 
     def generate_sql_query(self, natural_language_query):
         try:
-            prompt = f"{natural_language_query}, and cloumn names for different coloumns"
+            prompt = f"{natural_language_query}"
             
             prompt_template = ChatPromptTemplate.from_template(prompt)
             output_parser = StrOutputParser()
